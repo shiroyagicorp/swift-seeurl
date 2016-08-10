8db4b084f4a3e991bae16d7524d75c8d276c8504
Yusuke Ito <novi.mad@gmail.com>
2016-08-10 17:08:41 +0900

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,13 @@
 import PackageDescription
 
+#if os(macOS)
 let package = Package(
     name: "SeeURL",
-    dependencies: [
-        .Package(url: "https://github.com/novi/CcURL.git", majorVersion: 1),
-        .Package(url: "git@github.com:shiroyagicorp/swift-curl.git", majorVersion: 0)
-    ]
+    dependencies: [.Package(url: "git@github.com:shiroyagicorp/swift-curl.git", majorVersion: 0)]
 )
+#else
+let package = Package(
+    name: "SeeURL",
+    dependencies: [.Package(url: "https://github.com/novi/CcURL.git", majorVersion: 1)]
+)
+#endif
