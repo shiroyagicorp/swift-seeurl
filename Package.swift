785790384722055409c08645319c62be95c887d9
Yusuke Ito <novi.mad@gmail.com>
2017-03-07 18:57:25 +0900

    update for both Swift 3 and 3.1

--- Package.swift
+++ Package.swift
@@ -1,13 +1,9 @@
 import PackageDescription
 
-#if os(macOS)
 let package = Package(
     name: "SeeURL",
-    dependencies: [.Package(url: "git@github.com:shiroyagicorp/swift-curl.git", majorVersion: 0)]
+    targets: [
+        Target(name: "CcURL"),
+        Target(name: "SeeURL", dependencies: ["CcURL"])
+    ]
 )
-#else
-let package = Package(
-    name: "SeeURL",
-    dependencies: [.Package(url: "https://github.com/novi/CcURL.git", majorVersion: 1)]
-)
-#endif
